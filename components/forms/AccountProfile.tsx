42cac928cb572a9a69d76e1d46b72afe2c8604f6
itzray116R <rayhaanyaser@gmail.com>
2024-01-05 17:53:14 -0500

    Update files

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -12,8 +12,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {UserValidation} from "@/lib/validations/user";
+
+import Image from "next/image";
+import {ChangeEvent} from "react";
 
 interface Props {
   user :{
@@ -26,7 +30,7 @@ interface Props {
   }
   btnTitle: String;
 }
-const AccountProfile =(user, btnTitle) => {
+const AccountProfile =({user, btnTitle}:Props) => {
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -34,40 +38,145 @@ const AccountProfile =(user, btnTitle) => {
       profile_photo : '',
       name          : '',
       username      : '',
-      bio           : ''
+      bio           : '',
     }
   })
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) => {
+    e.preventDefault();
+  };
   function onSubmit(values: z.infer<typeof UserValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col justify-start gap-10">
+        className="flex flex-col justify-start gap-10"
+      >
         <FormField
           control={form.control}
-          name="username"
+          name="profile_photo"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="Profile Photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src="/assets/profile.svg"
+                    alt="Profile Photo"
+                    width={24}
+                    height={24}
+                    priority
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-dark-1 text-gray-200 border border-gray-300 p-2 rounded">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  className="account-form_image-Input"
+                  placeholder=" Upload a Image "
+                  onSubmit={(e)=> handleImage(e,field.onChange)}
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+              {/*<FormDescription>*/}
+              {/*  This is your public display name.*/}
+              {/*</FormDescription>*/}
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl className="flex-1 text-dark-1 text-gray-200 ">
+                <Input
+                  type={"text"}
+                  className="account-form_Input no-focus"
+                  {...field}
+                />
+              </FormControl>
+              {/*<FormDescription>*/}
+              {/*  This is your public display name.*/}
+              {/*</FormDescription>*/}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200 ">
+                <Input
+                  type={"text"}
+                  className="account-form_Input no-focus"
+                  {...field}
+                />
+              </FormControl>
+              {/*<FormDescription>*/}
+              {/*  This is your public display name.*/}
+              {/*</FormDescription>*/}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Bio
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200 ">
+                <Textarea
+                  rows={10}
+                  className="account-form_Input no-focus"
+                  {...field}
+                />
+              </FormControl>
+              {/*<FormDescription>*/}
+              {/*  This is your public display name.*/}
+              {/*</FormDescription>*/}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit">
+          Submit
+        </Button>
       </form>
     </Form>
-  )
+  );
+
+
 }
 
 export default AccountProfile;
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 export const UserValidation = z.object({
-  profile_photo : z.string().url().nonempty(),
+  profile_photo : z.string().url(),
   name          : z.string().min(3).max(40),
   username      : z.string().min(3).max(40),
   bio           : z.string().min(3).max(1000),
