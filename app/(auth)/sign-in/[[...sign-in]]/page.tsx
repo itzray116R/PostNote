ef3b5c014e55fd852bbeabe56c37997f24f28eaa
itzray116R <rayhaanyaser@gmail.com>
2024-01-09 10:58:51 -0500

    Update (root)

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import {SignIn} from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />;
+export default function Page () {
+  return (
+    <div className="origin-center">
+      <SignIn/>
+    </div>
+  );
 }
